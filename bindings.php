42d469c859cce6c2ac0e799806304c56e6135e7d
Bogdan <bmaletskyi@gmail.com>
2018-12-20 20:45:43 +0200

    remove not existing class

--- bindings.php
+++ bindings.php
@@ -4,7 +4,6 @@ return [
     App\Services\FilmServiceInterface::class                 => App\Services\FilmService::class,
     App\Services\StorageFormatServiceInterface::class        => App\Services\StorageFormatService::class,
     App\Services\ActorServiceInterface::class                => App\Services\ActorService::class,
-    App\Core\Database\DatabaseInterface::class               => App\Core\Database\MySQL::class,
     App\Repositories\FilmRepositoryInterface::class          => App\Repositories\FilmRepository::class,
     App\Repositories\StorageFormatRepositoryInterface::class => App\Repositories\StorageFormatRepository::class,
     App\Repositories\ActorRepositoryInterface::class         => App\Repositories\ActorRepository::class,
