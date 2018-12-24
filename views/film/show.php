6400104fcd33e0662ba8a8b7cff6470ea6ba6819
Bogdan <bmaletskyi@gmail.com>
2018-12-24 15:19:14 +0200

    add htmlspecialchars to title

--- views/film/show.php
+++ views/film/show.php
@@ -6,7 +6,7 @@
                 <h1 class="text-center my-4"><?= $film->title ?></h1>
 
                 <p>ID: <?= $film->id ?></p>
-                <p>Title: <?= $film->title ?></p>
+                <p>Title: <?= htmlspecialchars($film->title) ?></p>
                 <p>Release year: <?= $film->releaseYear ?></p>
                 <p>Format: <?= $film->storageFormat->name ?></p>
                 <p>Actors: <?= implode(', ', $film->actors) ?></p>
